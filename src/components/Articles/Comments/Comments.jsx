01ec0ec9c7ddb5d189b3cfaed792436f3b12adf1
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-06 12:58:51 +0100

    feedback to user when no comments are available

--- src/components/Articles/Comments/Comments.jsx
+++ src/components/Articles/Comments/Comments.jsx
@@ -1,6 +1,15 @@
 import SingleComment from "./SingleComment.jsx";
 
 export default function Comments({article, comments}){
+
+    if (comments.length === 0) {
+        return <section>
+            <h1 className={"title is-size-3 mb-6"}>Comments ({article.comment_count})</h1>
+            <p>There are no comments!</p>
+            <br/><br/>
+        </section>
+    }
+
     return <section>
         <h1 className={"title is-size-3 mb-6"}>Comments ({article.comment_count})</h1>
         {comments.map(comment => <SingleComment comment={comment} key={comment.comment_id}/>)}
