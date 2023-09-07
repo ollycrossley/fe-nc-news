d45bdb88923d7f6130855b6a5822852a3afa39a0
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-07 14:05:41 +0100

    makes base url configurable

--- api.js
+++ api.js
@@ -1,26 +1,29 @@
 import axios from "axios"
 
+// const baseURL = "https://olly-nc-news.onrender.com"
+const baseURL = "http://localhost:9090"
+
 export const getArticles = () => {
-    return axios.get("https://olly-nc-news.onrender.com/api/articles").then(response => response.data.articles)
+    return axios.get(`${baseURL}/api/articles`).then(response => response.data.articles)
 }
 
 export const getArticle = (article_id) => {
-    return axios.get(`https://olly-nc-news.onrender.com/api/articles/${article_id}`).then(response => response.data.article)
+    return axios.get(`${baseURL}/api/articles/${article_id}`).then(response => response.data.article)
 }
 
 export const getComments = (article_id) => {
-    return axios.get(`https://olly-nc-news.onrender.com/api/articles/${article_id}/comments`).then(response => response.data.comments)
+    return axios.get(`${baseURL}/api/articles/${article_id}/comments`).then(response => response.data.comments)
 }
 
 export const getUser = (username) => {
-    return axios.get(`https://olly-nc-news.onrender.com/api/users/${username}`).then(response => response.data.user)
+    return axios.get(`${baseURL}/api/users/${username}`).then(response => response.data.user)
 }
 
 export const patchVote = (article_id, vote) => {
-    return axios.patch(`https://olly-nc-news.onrender.com/api/articles/${article_id}`, {inc_votes: vote}).then(response => response.data.article)
+    return axios.patch(`${baseURL}/api/articles/${article_id}`, {inc_votes: vote}).then(response => response.data.article)
 }
 
 export const postComment = (article_id, username, body) => {
-    return axios.post(`https://olly-nc-news.onrender.com/api/articles/${article_id}/comments`, {username, body}).then(response => response.data.comment)
+    return axios.post(`${baseURL}/api/articles/${article_id}/comments`, {username, body}).then(response => response.data.comment)
 }
 
